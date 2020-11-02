b9d0c289bea5b62151f13ad2974104d33d3ef22b
HyperVS <63892536+HyperVS@users.noreply.github.com>
2020-11-02 15:26:55 -0500

    Add animals.c

---
+++ animals.c
@@ -0,0 +1,86 @@
+/* 
+Title: animals.c
+Description: This program sorts dogs and cats by age in ascending order and stores them in seperate output files.
+Author: Omar Abdelrahman
+*/
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// structure for animal
+struct animal{
+    char name[100];
+    char species[100];
+    char gender[10];
+    int age;
+    float weight;
+};
+
+// Function to sort animals based on age
+void sort_animals(struct animal list[], int n){
+
+    // Declaring variables
+    int i, j, min_index;
+
+    for (i = 0; i < n-1; i++){
+        // Find the minimum element in unsorted array
+        min_index = i;
+        for (j = i+1; j < n; j++)
+          if (list[j].age < list[min_index].age)
+            min_index = j;
+
+        // Swap the found minimum element with the first element
+        struct animal temp = list[min_index];
+        list[min_index] = list[i];
+        list[i] = temp;
+    }
+}
+
+// Main function
+int main(){
+    
+    int i, count = 0;
+    struct animal animals[200];
+    struct animal input;
+    char filename[100];
+    
+    printf("\nEnter the file name: ");
+    scanf("%s", filename);
+    printf("Output file name:\nsorted_dogs.txt\nsorted_cats.txt\n");
+
+    FILE *fp;
+    fp = fopen (filename, "r");
+    if (fp == NULL){
+        printf("Failed to open the input file.\n");
+        return 1;
+    }
+    
+    // Insert file contents to array and close the file
+    while(fscanf(fp, "%s %s %s %d %f", input.name, input.species, input.gender, &input.age, &input.weight)!=EOF)
+        animals[count++] = input;
+    fclose (fp);
+
+    // Call the sort_animals function
+    sort_animals(animals, count);
+
+    // Open files for writing
+    FILE *out1, *out2;
+    out1 = fopen("sorted_dogs.txt", "w");
+    out2 = fopen("sorted_cats.txt", "w");
+
+    for(i=0; i<count; i++){
+
+        if(strcmp(animals[i].species, "dog")==0){
+            fprintf(out1, "%s\t%s\t%d\t%.2f\n", animals[i].name, animals[i].gender, animals[i].age, animals[i].weight);
+        }
+
+        else if(strcmp(animals[i].species, "cat")==0){
+            fprintf(out2, "%s\t%s\t%d\t%.2f\n", animals[i].name, animals[i].gender, animals[i].age, animals[i].weight);
+        }
+    }
+    
+    fclose(out1);
+    fclose(out2);
+    return 0;
+}
